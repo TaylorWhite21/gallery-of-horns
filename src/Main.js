24b43b70c0f11f4090844dafe2368fc94edd7ed8
TaylorWhite21 <taylorwhite2190@gmail.com>
2021-07-12 22:29:20 -0400

    moved title, imageurl, description to main from App

--- src/Main.js
+++ src/Main.js
@@ -2,15 +2,21 @@ import React from 'react';
 import HornedBeast from './HornedBeast.js';
 
 class Main extends React.Component {
-  render () {
+  render() {
     return (
-      <HornedBeast
-        title={this.props.title}
-        imageUrl={this.props.imageUrl}
-        description={this.props.description}
-      />
+      <>
+        <HornedBeast
+          title="Eri from MHA"
+          imageUrl="https://i.quotev.com/img/q/u/20/8/25/tmw4ykbro3.jpg"
+          description="Happy Eri with horn"
+        />
 
-      
+        <HornedBeast
+          title="Imp Mercy"
+          imageUrl="https://pm1.narvii.com/6828/bb90e61a8b0de7d2133c9aa82e3007897c721c26v2_hq.jpg"
+          description="Imp Mercy from Overwatch"
+        />
+      </>
     )
   }
 }
